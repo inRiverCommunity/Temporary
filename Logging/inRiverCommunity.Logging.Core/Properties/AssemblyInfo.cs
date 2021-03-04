6490f40ce7d34dbe95febe339277da0c60647c79
Roy Eriksson <mail@royeriksson.se>
2021-03-04 08:13:26 +0100

    Removed (stupid) default AssemblyInfo text
    I'll change the regexp keys another day...

--- Logging/inRiverCommunity.Logging.Core/Properties/AssemblyInfo.cs
+++ Logging/inRiverCommunity.Logging.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("inRiverCommunity.Logging.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("inRiverCommunity.Logging.Core")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
